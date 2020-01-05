a75486f995c003be887c0cf51187d580722907c1
Andres Galindo <andres.galindo@flexshopper.com>
2020-01-05 18:29:49 -0500

    Implement array access and updated README

--- README.md
+++ README.md
@@ -24,3 +24,30 @@ $client = new \Zulip\Client($config);
 $config = \Zulip\Config::fromFile(); // if no argument is provided it defaults to ~/zuliprc
 $client = new \Zulip\Client($config);
 ```
+### Examples
+
+#### Sending a message
+```php
+$client = new \Zulip\Client();
+$response = $client->messages->send([
+    'to' => 'general',
+    'type' => 'stream',
+    'topic' => 'test',
+    'content' => "This is a test"
+]);
+
+if ($response->isSuccessful()) {
+    // We sent a message!
+}
+```
+#### Uploading a file and using it in a message
+```php
+$client = new \Zulip\Client($config);
+$response = $client->messages->uploadFile(['filename' => '/tmp/dog.jpg']);
+$client->messages->send([
+    'to' => 'general',
+    'type' => 'stream',
+    'topic' => 'test',
+    'content' => "This is a test of the lib functionality [PUPPY](${response['uri']})"
+]);
+```
--- src/Resource/ApiResponse.php
+++ src/Resource/ApiResponse.php
@@ -4,8 +4,9 @@ namespace Zulip\Resource;
 
 use GuzzleHttp\Psr7\Stream;
 use Psr\Http\Message\ResponseInterface;
+use Zulip\Exception;
 
-class ApiResponse
+class ApiResponse implements \ArrayAccess
 {
     private $rawResponse;
 
@@ -23,10 +24,49 @@ class ApiResponse
     {
         $body = $this->rawResponse->getBody();
         if ($body instanceof Stream) {
-            return json_decode($body->getContents());
+            return json_decode($body->getContents(), true);
         }
 
         return $this->rawResponse->getBody();
+    }
+
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        $data = $this->getData();
+        return array_key_exists($offset, $data);
+    }
+
+    /**
+     * @param mixed $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        $data = $this->getData();
+        return $data[$offset];
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @throws Exception
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw new Exception('Cannot set values on a response');
+    }
+
+    /**
+     * @param mixed $offset
+     * @throws Exception
+     */
+    public function offsetUnset($offset)
+    {
+        throw new Exception('Cannot unset values on a response');
     }
 
     /**
